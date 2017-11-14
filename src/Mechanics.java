4963af92c05d4c0c0fe8ff9cd49ac14e823582a3
Manavivian <h.vivian92@gmail.com>
2017-11-14 12:25:34 -0800

    ArrayOutofBounds Fixed, the true and false of move is now functional

--- src/Mechanics.java
+++ src/Mechanics.java
@@ -35,6 +35,7 @@ public class Mechanics {
 		int pivot = location;
 		pivot++;
 		int last = 0;
+		int check = pivot %7;
 		while (hand > 0) {
 			if (((pivot / 7) != side && pivot == 6) || ((pivot / 7) != side && pivot == 13)) {
 				pivot++;
@@ -43,12 +44,10 @@ public class Mechanics {
 				pivot = 0;
 			hand--;
 			last = pivot;
+			check = pivot/7;
 			board[pivot++] += 1;
 		}
-		int check = pivot % 7;
-		if(pivot == 14)
-			pivot = 0;
-		if ((check == side && last == 6) && (check == side && last == 13))
+		if ((check == side && last == 6) || (check == side && last == 13))
 			return true;
 		else if ((board[pivot] == 1) && ((pivot % 7) == side)) {
 			steal(last, side);
@@ -137,7 +136,7 @@ public class Mechanics {
 	public static void main(String[] args) {
 		Mechanics board = new Mechanics(4);
 		System.out.println(board.toString());
-		System.out.println(board.move(9));
+		System.out.println(board.move(3));
 		System.out.println("Results: ");
 		System.out.println(board.toString());
 	}
